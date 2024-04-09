83393572f328405da37c97ba6266116d1f48bb12
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-04-09 10:04:58 -0400

    directory and file setup

---
+++ data/api/appleMusic.js
@@ -0,0 +1,11 @@
+/**
+ * funcs for apple music api
+ */
+
+import axios from "axios";
+
+const MOD_NAME = "data/api/appleMusic.js";
+
+const exportedMethods = {};
+
+export default exportedMethods;
---
+++ data/api/spotify.js
@@ -0,0 +1,11 @@
+/**
+ * funcs for spotify api
+ */
+
+import axios from "axios";
+
+const MOD_NAME = "data/api/spotify.js";
+
+const exportedMethods = {};
+
+export default exportedMethods;
