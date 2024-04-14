48c3b49c01b2b75683e4ea921695e801c165f371
ehodor <105980084+ehodor@users.noreply.github.com>
2024-04-14 12:52:53 -0400

    Added birthday dropdown selectors to signup menu and date validation

--- public/js/signup_validation.js
+++ public/js/signup_validation.js
@@ -1,5 +1,18 @@
+import moment from moment
+
 
 $('#signup').submit((event) => {
     event.preventDefault();
+    const userMonth =  $('#user_month').find(":selected").val();
+    const userDay = $('#user_day').find(":selected").val();
+    const userYear = $('#user_year').find(":selected").val();
+    const userBirth = `${userMonth}/${userDay}/${userYear}`;
+    if (!moment(userBirth, 'MM/DD/YYYY').isValid()) {
+        //add error
+    }
+    var yearsOld = moment().diff(userBirth, 'years');
+    if (yearsOld < 13) {
+        //add error
+    }
     /*finish adding client-side validation here */
 })
