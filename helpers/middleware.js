ec55c78c429d9af2671ffe85edbf25c696b13a49
ehodor <105980084+ehodor@users.noreply.github.com>
2024-04-19 19:14:40 -0400

    added basic middleware rerouting functions

---
+++ helpers/middleware.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import session from 'express-session';
+import authRoutes from '../routes/index.js'
+
+const rootReroute = (req, res, next) => { //for root url
+    if (req.session.user) {
+        return res.render('feed', {});
+    }
+}
+
+const loginSignupReroute = (req, res, next) => {// use for login + signup pages
+    if (req.session.user) {
+        res.redirect('/');
+    }
+}
+
+
+export {rootReroute, loginSignupReroute};
