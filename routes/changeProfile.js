3c89ce9806b1801bc597a37a765ba3243fad0132
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-04-24 15:35:02 -0400

    bug fix: add user._id to req.session.user

--- routes/changeProfile.js
+++ routes/changeProfile.js
@@ -1,10 +1,8 @@
-
 import { Router } from "express";
 import { userData } from "../data/index.js";
 import vld from "../helpers/validation.js";
 import errorMessage from "../helpers/error.js";
 import { users, posts } from "../config/mongoCollections.js";
-
 
 const router = Router();
 
@@ -15,7 +13,7 @@ router
     })
     .post(async (req, res) => {
         const changedData = req.body;
-        const updates ={};
+        const updates = {};
         try {
             if (
                 changedData.publicProfile == "public" ||
@@ -24,19 +22,25 @@ router
                 updates[publicProfile] = changedData.publicProfile;
             }
             if (changedData.changeDisplay.length > 0) {
-                changedData.changeDisplay = vld.returnValidString(changedData.changeDisplay);
+                changedData.changeDisplay = vld.returnValidString(
+                    changedData.changeDisplay
+                );
                 updates[name] = changedData.changeDisplay;
-                }
+            }
             if (changedData.changeUsername.length > 0) {
                 updates[username] = changedData.changeUsername;
             }
             if (changedData.changePassword.length > 0) {
                 updates[password] = changedData.changePassword;
             }
-            const updated = await userData.updateUser(req.session.user._id, updates);
+            const updated = await userData.updateUser(
+                req.session.user._id,
+                updates
+            );
             return updated;
-            } catch {
-                return res.status(400).json({ error: e });
-            }
+        } catch {
+            return res.status(400).json({ error: e });
         }
-    )
+    });
+
+export default router;
