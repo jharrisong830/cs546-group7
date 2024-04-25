2095bc02d21b04ddf5fb87690143514dcbb53d74
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-04-25 11:53:32 -0400

    fixes from ajax push

--- routes/feedapi.js
+++ routes/feedapi.js
@@ -1,20 +1,23 @@
-
-import {Router} from 'express';
+import { Router } from "express";
 const router = Router();
-import xss from 'xss';
+import xss from "xss";
 import { postData } from "../data/index.js";
 
 router
-    .route('/api/post/json')
+    .route("/post/json")
     .get(async (req, res) => {
-        const feedPosts = await postData.generateFeed(req.session.user._id); 
+        const feedPosts = await postData.generateFeed(req.session.user._id);
         res.json(feedPosts);
     })
     .post(async (req, res) => {
         let cleanPost = xss(req.body.post);
         //upload post to DB
-        console.log(cleanPost);
-        res.json({success: true, post:cleanPost, user: req.session.user.username});
-    })
+        // console.log(cleanPost);
+        res.json({
+            success: true,
+            post: cleanPost,
+            user: req.session.user.username
+        });
+    });
 
 export default router;
