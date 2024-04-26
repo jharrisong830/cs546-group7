5d50fb9c541eb8c59faa86f13700c1678822a266
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-04-26 16:16:49 -0400

    issues with duplicate clicks for selector

--- public/js/playlist_data_modal.js
+++ public/js/playlist_data_modal.js
@@ -1,51 +1,172 @@
 $("#postModal").on("shown.bs.modal", (event) => {
     // when the modal is shown...
-    const requestConfig = {
-        method: "GET",
-        url: "/api/music/spotify/playlists" // just playlists for now, add buttons once working
-    };
-
     const outerText = $("#textContentOuter").val().trim();
     $("#textContentInner").val(outerText); // set the value of the text area to be whatever the user was typing before
-    $.ajax(requestConfig).then(
-        (response) => {
-            if (response.playlists.length === 0) {
-                $("#error").html(
-                    "We couldn't find any playlists. Add some playlists to your library and try again."
-                );
-                $("#error").removeAttr("hidden");
-                $("#playlistLoading").attr("hidden", true);
-                $(".spinner-border").attr("hidden", true);
-            } else {
-                response.playlists.forEach((pl) => {
-                    // begin to generate html for each playlist (will be radio buttons)
-                    const newPlaylistOption = $(`
-                    <div class="form-check">
-                        <input required type="radio" id="${pl._id}" value="${pl._id}" name="musicContentId" class="form-check-input">
-                        <label for="${pl._id}" class="form-check-label">
-                            <img class="" alt="${pl.name} playlist thumbnail" src="${pl.thumbnailURL}">
-                            <br>
-                            ${pl.name}
-                        </label>
-                    </div>
-                    `);
-                    $("#playlistSelector").append(newPlaylistOption); // add the playlist html to the div
-                });
-                $("#playlistLoading").attr("hidden", true);
-                $(".spinner-border").attr("hidden", true);
-                $("#playlistSelector").removeAttr("hidden");
-            }
-        },
-        (responseError) => {
-            // triggered if an http error code is received (from jquery docs, responseError is a different object than our api's response)
+    // $("#playlists").attr("checked", true); // reset to the playlist view on each showing of the modal
+    $("#musicContentTypeButtons").off(); // remove all event handlers from the buttons (otherwise they start to stack lol)
+
+    $("#musicContentTypeButtons").click((event) => {
+        // on click of the button group (listens on all buttons in the container)...
+        $("#contentLoading").removeAttr("hidden"); // render loading when clicked, and clear all output
+        $(".spinner-border").removeAttr("hidden");
+
+        $("#error").html("");
+        $("#error").attr("hidden", true);
+
+        $("#contentSelector").html("");
+        $("#contentSelector").attr("hidden", true);
+
+        const currChecked = $('input[name="musicContentType"]:checked')
+            .val()
+            .trim()
+            .toLowerCase(); // get the checked option (determines the view)
+        if (currChecked === "playlists") {
+            // render playlists
+            const requestConfig = {
+                method: "GET",
+                url: "/api/music/spotify/playlists"
+            };
+            $.ajax(requestConfig).then(
+                (response) => {
+                    if (response.playlists.length === 0) {
+                        $("#error").html(
+                            "We couldn't find any playlists. Add some playlists to your library and try again."
+                        );
+                        $("#error").removeAttr("hidden");
+                        $("#contentLoading").attr("hidden", true);
+                        $(".spinner-border").attr("hidden", true);
+                    } else {
+                        response.playlists.forEach((pl) => {
+                            // begin to generate html for each playlist (will be radio buttons)
+                            const newPlaylistOption = $(`
+                            <div class="form-check">
+                                <input required type="radio" id="${pl._id}" value="${pl._id}" name="musicContentId" class="form-check-input">
+                                <label for="${pl._id}" class="form-check-label">
+                                    <img class="" alt="${pl.name} playlist thumbnail" src="${pl.thumbnailURL}">
+                                    <br>
+                                    ${pl.name}
+                                </label>
+                            </div>
+                            `);
+                            $("#contentSelector").append(newPlaylistOption); // add the playlist html to the div
+                        });
+                        $("#contentLoading").attr("hidden", true);
+                        $(".spinner-border").attr("hidden", true);
+                        $("#contentSelector").removeAttr("hidden");
+                    }
+                },
+                (responseError) => {
+                    // triggered if an http error code is received (from jquery docs, responseError is a different object than our api's response)
+                    $("#error").html(
+                        `There was an error fetching your playlists. Please try again. (Error message: ${responseError.responseJSON.errmsg})`
+                    );
+                    $("#error").removeAttr("hidden");
+                    $("#contentLoading").attr("hidden", true);
+                    $(".spinner-border").attr("hidden", true);
+                }
+            );
+        } else if (currChecked === "songs") {
+            const requestConfig = {
+                method: "GET",
+                url: "/api/music/spotify/playlists" // just playlists for now, add buttons once working
+            };
+            $.ajax(requestConfig).then(
+                // renders playlists by default
+                (response) => {
+                    if (response.playlists.length === 0) {
+                        $("#error").html(
+                            "We couldn't find any playlists. Add some playlists to your library and try again."
+                        );
+                        $("#error").removeAttr("hidden");
+                        $("#contentLoading").attr("hidden", true);
+                        $(".spinner-border").attr("hidden", true);
+                    } else {
+                        response.playlists.forEach((pl) => {
+                            // begin to generate html for each playlist (will be radio buttons)
+                            const newPlaylistOption = $(`
+                            <div class="form-check">
+                                <input required type="radio" id="${pl._id}" value="${pl._id}" name="musicContentId" class="form-check-input">
+                                <label for="${pl._id}" class="form-check-label">
+                                    <img class="" alt="${pl.name} playlist thumbnail" src="${pl.thumbnailURL}">
+                                    <br>
+                                    ${pl.name}
+                                </label>
+                            </div>
+                            `);
+                            $("#contentSelector").append(newPlaylistOption); // add the playlist html to the div
+                        });
+                        $("#contentLoading").attr("hidden", true);
+                        $(".spinner-border").attr("hidden", true);
+                        $("#contentSelector").removeAttr("hidden");
+                    }
+                },
+                (responseError) => {
+                    // triggered if an http error code is received (from jquery docs, responseError is a different object than our api's response)
+                    $("#error").html(
+                        `There was an error fetching your playlists. Please try again. (Error message: ${responseError.responseJSON.errmsg})`
+                    );
+                    $("#error").removeAttr("hidden");
+                    $("#contentLoading").attr("hidden", true);
+                    $(".spinner-border").attr("hidden", true);
+                }
+            );
+        } else if (currChecked === "albums") {
+            const requestConfig = {
+                method: "GET",
+                url: "/api/music/spotify/playlists" // just playlists for now, add buttons once working
+            };
+            $.ajax(requestConfig).then(
+                // renders playlists by default
+                (response) => {
+                    if (response.playlists.length === 0) {
+                        $("#error").html(
+                            "We couldn't find any playlists. Add some playlists to your library and try again."
+                        );
+                        $("#error").removeAttr("hidden");
+                        $("#contentLoading").attr("hidden", true);
+                        $(".spinner-border").attr("hidden", true);
+                    } else {
+                        response.playlists.forEach((pl) => {
+                            // begin to generate html for each playlist (will be radio buttons)
+                            const newPlaylistOption = $(`
+                            <div class="form-check">
+                                <input required type="radio" id="${pl._id}" value="${pl._id}" name="musicContentId" class="form-check-input">
+                                <label for="${pl._id}" class="form-check-label">
+                                    <img class="" alt="${pl.name} playlist thumbnail" src="${pl.thumbnailURL}">
+                                    <br>
+                                    ${pl.name}
+                                </label>
+                            </div>
+                            `);
+                            $("#contentSelector").append(newPlaylistOption); // add the playlist html to the div
+                        });
+                        $("#contentLoading").attr("hidden", true);
+                        $(".spinner-border").attr("hidden", true);
+                        $("#contentSelector").removeAttr("hidden");
+                    }
+                },
+                (responseError) => {
+                    // triggered if an http error code is received (from jquery docs, responseError is a different object than our api's response)
+                    $("#error").html(
+                        `There was an error fetching your playlists. Please try again. (Error message: ${responseError.responseJSON.errmsg})`
+                    );
+                    $("#error").removeAttr("hidden");
+                    $("#contentLoading").attr("hidden", true);
+                    $(".spinner-border").attr("hidden", true);
+                }
+            );
+        } else {
+            // hopefully shouldn't hit this...
             $("#error").html(
-                `There was an error fetching your playlists. Please try again. (Error message: ${responseError.responseJSON.errmsg})`
+                `Current checked item is ${currChecked}... I don't think you're supposed to be here`
             );
             $("#error").removeAttr("hidden");
-            $("#playlistLoading").attr("hidden", true);
+            $("#contentLoading").attr("hidden", true);
             $(".spinner-border").attr("hidden", true);
         }
-    );
+    });
+
+    // $("#playlists").trigger("click"); // click on playlists option to render them by default
 });
 
 $("#postModal").on("hidden.bs.modal", (event) => {
@@ -55,12 +176,12 @@ $("#postModal").on("hidden.bs.modal", (event) => {
 
     // now, reset all of the fields
 
-    $("#playlistLoading").removeAttr("hidden");
+    $("#contentLoading").removeAttr("hidden");
     $(".spinner-border").removeAttr("hidden");
 
     $("#error").html("");
     $("#error").attr("hidden", true);
 
-    $("#playlistSelector").html("");
-    $("#playlistSelector").attr("hidden", true);
+    $("#contentSelector").html("");
+    $("#contentSelector").attr("hidden", true);
 });
