16c5938b3c3a79e7ab47134c1d51328dec12f343
Ramses Peralta <153132353+RamsesPeralta7@users.noreply.github.com>
2024-04-29 20:18:20 -0400

    Added dropdown for tags

--- public/js/playlist_data_modal.js
+++ public/js/playlist_data_modal.js
@@ -1,11 +1,23 @@
 
-// Found a huge array of genres we could use
-// For now I will just allow users to put in text for tags on their posts
-// https://developer.spotify.com/documentation/web-api/reference/get-recommendation-genres
+// Array from: https://developer.spotify.com/documentation/web-api/reference/get-recommendation-genres
+// Now changed so tags are chosen from a dropdown of genres
 
-const genres = ["acoustic","afrobeat","alt-rock","alternative","ambient","anime","black-metal","bluegrass","blues","bossanova","brazil","breakbeat","british","cantopop","chicago-house","children","chill","classical","club","comedy","country","dance","dancehall","death-metal","deep-house","detroit-techno","disco","disney","drum-and-bass","dub","dubstep","edm","electro","electronic","emo","folk","forro","french","funk","garage","german","gospel","goth","grindcore","groove","grunge","guitar","happy","hard-rock","hardcore","hardstyle","heavy-metal","hip-hop","holidays","honky-tonk","house","idm","indian","indie","indie-pop","industrial","iranian","j-dance","j-idol","j-pop","j-rock","jazz","k-pop","kids","latin","latino","malay","mandopop","metal","metal-misc","metalcore","minimal-techno","movies","mpb","new-age","new-release","opera","pagode","party","philippines-opm","piano","pop","pop-film","post-dubstep","power-pop","progressive-house","psych-rock","punk","punk-rock","r-n-b","rainy-day","reggae","reggaeton","road-trip","rock","rock-n-roll","rockabilly","romance","sad","salsa","samba","sertanejo","show-tunes","singer-songwriter","ska","sleep","songwriter","soul","soundtracks","spanish","study","summer","swedish","synth-pop","tango","techno","trance","trip-hop","turkish","work-out","world-music"];
+const genresArray = ["acoustic","afrobeat","alt-rock","alternative","ambient","anime","black-metal","bluegrass","blues","bossanova","brazil","breakbeat","british","cantopop","chicago-house","children","chill","classical","club","comedy","country","dance","dancehall","death-metal","deep-house","detroit-techno","disco","disney","drum-and-bass","dub","dubstep","edm","electro","electronic","emo","folk","forro","french","funk","garage","german","gospel","goth","grindcore","groove","grunge","guitar","happy","hard-rock","hardcore","hardstyle","heavy-metal","hip-hop","holidays","honky-tonk","house","idm","indian","indie","indie-pop","industrial","iranian","j-dance","j-idol","j-pop","j-rock","jazz","k-pop","kids","latin","latino","malay","mandopop","metal","metal-misc","metalcore","minimal-techno","movies","mpb","new-age","new-release","opera","pagode","party","philippines-opm","piano","pop","pop-film","post-dubstep","power-pop","progressive-house","psych-rock","punk","punk-rock","r-n-b","rainy-day","reggae","reggaeton","road-trip","rock","rock-n-roll","rockabilly","romance","sad","salsa","samba","sertanejo","show-tunes","singer-songwriter","ska","sleep","songwriter","soul","soundtracks","spanish","study","summer","swedish","synth-pop","tango","techno","trance","trip-hop","turkish","work-out","world-music"];
 
 let tagCount = 1;
+
+function populateDropdown(genres) {
+    $(".form-control.mt-2.genre-dropdown").each(function() {
+        const select = $(this);
+        select.empty(); // Clear current options
+        select.append($('<option>', { value: "", text: "Select a genre..." }));
+        genres.forEach(genre => {
+            select.append($('<option>', { value: genre, text: genre }));
+        });
+    });
+}
+
+populateDropdown(genresArray);
 
 $("#submitSearchCatalog").click((event) => {
     // song search bar (mimics a form)
@@ -105,7 +117,7 @@ $("#postModal").on("shown.bs.modal", (event) => {
             $('.tag-label').removeAttr('hidden');
             $('#customTags').removeAttr('hidden');
 
-            if (tagCount < 3){
+            if (tagCount < 3) {
                 $('#addTagButton').removeAttr('hidden');
             }
 
@@ -208,7 +220,6 @@ $("#postModal").on("hidden.bs.modal", (event) => {
     // now, reset all of the fields
 
     for (let i = 1; i <= 3; i++) {
-        $(`#customTag${i}`).val('');
         if(i !== 1){
             $(`#customTag${i}`).attr('hidden', true);
         }
